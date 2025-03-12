2fb4d6210f1a7a4d7d6b291c50f3b592c7418290
Gianluca Caregnato <gianluca.caregnato@gmail.com>
2025-03-12 16:16:40 +0100

    New feature added to Task5

--- LAB-1/Task5.cpp
+++ LAB-1/Task5.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char** argv){
 
-    cv::Mat VerticalGradient(256,256,CV_8UC1);
-    cv::Mat HorizontalGradient(256,256,CV_8UC1);
+    cv::Mat VerticalGradient(256, 256, CV_8UC1);
+    cv::Mat HorizontalGradient(256, 256, CV_8UC1);
 
     for(int x = 0; x < 256; x++){
         for(int y = 0; y < 256; y++){
@@ -22,5 +22,26 @@ int main(int argc, char** argv){
     cv::imwrite("vertical_gradient.jpg", VerticalGradient);
     cv::imwrite("horizontal_gradient.jpg", HorizontalGradient);
 
+
+    cv::Mat Chess1(300, 300, CV_8UC1);
+    cv::Mat Chess2(300, 300, CV_8UC1);
+
+    for(int x = 0; x < 300; x++){
+        for(int y = 0; y < 300; y++){
+            Chess1.at<uchar>(x,y) = 255*((x/20 + y/20) % 2);
+            Chess2.at<uchar>(x,y) = 255*((x/50 + y/50) % 2);
+        }
+    }
+
+    cv::namedWindow("Chess1");
+    cv::namedWindow("Chess2");
+
+    cv::imshow("Chess1",Chess1);
+    cv::imshow("Chess2", Chess2);
+
+    cv::imwrite("Chess1.jpg", Chess1);
+    cv::imwrite("Chess2.jpg", Chess2);
+
     cv::waitKey(0);
+    return 0;
 }
