d7e4caebf2357834fb18c37fd248edcfc4341d65
Gianluca Caregnato <gianluca.caregnato@gmail.com>
2025-03-13 10:52:06 +0100

    some progress with Task6

--- LAB-1/Task6.cpp
+++ LAB-1/Task6.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
 int main(int argc, char** argv){
 
@@ -8,18 +9,20 @@ int main(int argc, char** argv){
         return 0;
     }
 
-    cv::Mat img = cv::imread(argv[1]).clone();
+    cv::Mat img = cv::imread(argv[1]);
     if (img.empty()) {
         std::cout << "File name is wrong or the file does not exist\n";
         return 0;
     }
-
+    
     cv::Mat gray;
     cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
 
     cv::namedWindow("Example 1");
 	cv::imshow("Example 1", gray);
 	cv::waitKey(0);
+
+    return 0;
     
 
     return 0;
